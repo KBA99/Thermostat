4cc80b2687cb5e36676f03d18514dd873d8cf557
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-04-15 15:20:10 +0100

    WIP - Fetch Weather

--- src/weather.js
+++ src/weather.js
@@ -1,53 +1,13 @@
 
 // Hardcodeded result of fetch request
 let getPostData = () => {
-  return {
-      "coord":{
-         "lon":-0.1257,
-         "lat":51.5085
-      },
-      "weather":[
-         {
-            "id":500,
-            "main":"Rain",
-            "description":"light rain",
-            "icon":"10d"
-         }
-      ],
-      "base":"stations",
-      "main":{
-         "temp":9.27,
-         "feels_like":6.43,
-         "temp_min":8.33,
-         "temp_max":10,
-         "pressure":1033,
-         "humidity":53
-      },
-      "visibility":10000,
-      "wind":{
-         "speed":5.66,
-         "deg":70
-      },
-      "rain":{
-         "1h":0.2
-      },
-      "clouds":{
-         "all":99
-      },
-      "dt":1618493803,
-      "sys":{
-         "type":1,
-         "id":1414,
-         "country":"GB",
-         "sunrise":1618463060,
-         "sunset":1618512975
-      },
-      "timezone":3600,
-      "id":2643743,
-      "name":"London",
-      "cod":200
-  }
-}
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid=a0fe58708e6d694a289790f09cf8e633&units=metric").then(response => {
+    response.json().then(json => {
+      // We have the JSON!
+      return json;
+    });
+  });
+};
     
 let renderPost = (postData) => {
   let postDataTemp = `${postData.main.temp}`
