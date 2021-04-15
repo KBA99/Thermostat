8e95f2eab25245b508d78a038f365765ef63138f
Dominic-Taylor-Dev <email.dom.taylor@gmail.com>
2021-04-15 16:37:00 +0100

    working but without backend

--- src/weather.js
+++ src/weather.js
@@ -1,20 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-// Hardcodeded result of fetch request
-let getPostData = () => {
-  fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid=a0fe58708e6d694a289790f09cf8e633&units=metric").then(response => {
-    response.json().then(json => {
-      // We have the JSON!
-      return json;
-    });
-  });
-};
-    
-let renderPost = (postData) => {
-  let postDataTemp = `${postData.main.temp}`
-  return postDataTemp
-}
+  document.querySelector('#city-selector').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const city = document.querySelector('#city').value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a0fe58708e6d694a289790f09cf8e633&units=metric`
 
-let post = getPostData();
-let rendered = renderPost(post);
+    fetch(url).then(response => {
+      response.json().then(post => {
+        // ...to here.
+        let rendered = renderPost(post);
+        document.getElementById("weatherapi").innerHTML = rendered;
+      })
+    })
+  })
 
-console.log(rendered)
+  function getPostData() {
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid=a0fe58708e6d694a289790f09cf8e633&units=metric").then(response => {
+      response.json().then(post => {
+        // ...to here.
+        let rendered = renderPost(post);
+        document.getElementById("weatherapi").innerHTML = rendered;
+      })
+    })
+  }
+
+  function renderPost(postData) {
+    let cityHTML = `<h2>API city: ${postData.name}</h2>`
+    let tempHTML = `<p>API temp: ${postData.main.temp}</p>`;
+    return cityHTML + tempHTML;
+  }
+
+  getPostData();
+
+});
